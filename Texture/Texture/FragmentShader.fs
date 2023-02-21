ca80e29e19f57223bac656b2463b0988e9335a18
yeopgi <7677942@hanmail.net>
2023-02-21 15:01:54 +0900

    update

--- Texture/Texture/FragmentShader.fs
+++ Texture/Texture/FragmentShader.fs
@@ -4,11 +4,10 @@ out vec4 FragColor;
 in vec3 ourColor;
 in vec2 TexCoord;
 
-uniform sampler2D ourTexture;
 uniform sampler2D texture1;
 uniform sampler2D texture2;
 
 void main()
 {
-    FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
+    FragColor = mix(texture(texture1, TexCoord), texture(texture2, -TexCoord), 0.2);
 }
